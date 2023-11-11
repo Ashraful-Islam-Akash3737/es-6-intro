59efd871d2239c79495a9d6b9f8b8574e36bb356
Ashraful-Islam-Akash <abirashrafulislamabir@gmail.com>
2023-11-11 22:08:07 +0600

    create template.js file to know about the use of backtick

---
+++ template.js
@@ -0,0 +1,15 @@
+const a = 20;
+const b = 30;
+const result ="the sum of " +  a  + " and " + b + " is: " + (a + b);
+console.log(result);
+
+// ---------------with `` or back-tick to write email, easy, or etc -------------
+
+const ab = 20;
+const bc = 30;
+const result1 =`the sum of ${a} and ${b} is: ${a+b}`;
+console.log(result1);
+
+const email = `Hi, Akash
+My name is also Akash`;
+console.log(email);
